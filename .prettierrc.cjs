bb99c1b0ecf037b1fca3ef3dde169e09a161713e
PedroGomes <pgomes@ultragenyx.com>
2023-04-17 17:22:15 +0100

    More configs and remove useless code

---
+++ .prettierrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  quoteProps: "as-needed",
+  trailingComma: "all",
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: "always",
+};
